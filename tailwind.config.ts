4f01ff1393eb3ec0cf06f20f089eff9f12d2a1e8
amanda-santos <amandasf0200@gmail.com>
2023-10-10 19:10:19 -0300

    feat: ✨ create form fields

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
         profile: 'max-content 1fr max-content',
+        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
     },
   },
