133f814388a969da0079f5482b162f3f4da9ddc4
amanda-santos <amandasf0200@gmail.com>
2023-10-13 19:57:32 -0300

    feat: ✨ add preview of uploaded image

--- src/components/Form/FileInput/Control.tsx
+++ src/components/Form/FileInput/Control.tsx
@@ -1,8 +1,31 @@
+'use client'
 
-import { ComponentProps } from 'react'
+import { ChangeEvent, ComponentProps } from 'react'
+
+import { useFileInput } from './Root'
 
 export type ControlProps = ComponentProps<'input'>
 
 export function Control(props: ControlProps) {
-  return <input type="file" className="sr-only" id="photo" {...props} />
+  const { id, onFilesSelected } = useFileInput()
+
+  function handleFilesSelected(event: ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files?.length) {
+      return
+    }
+
+    const files = Array.from(event.target.files)
+
+    onFilesSelected(files)
+  }
+
+  return (
+    <input
+      type="file"
+      className="sr-only"
+      id={id}
+      onChange={handleFilesSelected}
+      {...props}
+    />
+  )
 }
