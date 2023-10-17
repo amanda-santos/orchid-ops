bed9cb54978a665d2717b825eaf7f54c8ba87d52
amanda-santos <amandasf0200@gmail.com>
2023-10-17 17:49:46 -0300

    feat: 💄 add animation on Select field opening

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,6 +27,18 @@ const config: Config = {
       maxWidth: {
         app: '700px',
       },
+
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: '0', transform: 'translateY(-2px)' },
+          to: { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+
+      animation: {
+        slideDownAndFade:
+          'slideDownAndFade 400ms cubic-bezier(0.16, 1, 0.3, 1)',
+      },
     },
   },
   plugins: [],
