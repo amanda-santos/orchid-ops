410c7df6c6b838867c323cfbe5ae0faf3cf2c45e
amanda-santos <amandasf0200@gmail.com>
2023-10-17 17:44:18 -0300

    feat: 💄 add animation on file list

--- src/components/Form/FileInput/FileList.tsx
+++ src/components/Form/FileInput/FileList.tsx
@@ -1,15 +1,17 @@
 'use client'
 
 import { Trash2, UploadCloud } from 'lucide-react'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 import { useFileInput } from './Root'
 import { formatBytes } from '@/utils/formatBytes'
 
 export function FileList() {
   const { files } = useFileInput()
+  const [parent] = useAutoAnimate()
 
   return (
-    <div className="mt-4 space-y-3">
+    <div ref={parent} className="mt-4 space-y-3">
       {files.map((file) => {
         return (
           <div
